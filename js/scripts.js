47c7371344bfc9efeac72fef296a27e43e90580f
Ewa <grtxhu@gmail.com>
2018-05-15 11:27:51 +0200

    Modify files

--- js/scripts.js
+++ js/scripts.js
@@ -5,33 +5,24 @@ function Phone(brand, price, color, operatingSystem) {
     this.operatingSystem = operatingSystem;
 }
 
-Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " ,price is " + this.price + ", and the operatingSystem is " + this.operatingSystem)
-}
-
-
-
 
 var iPhone6S = new Phone("Apple", 2250, "silver", "iOS");
 
-iPhone6S.printInfo();
 
 var SamsungGalaxyS6 = new Phone("Samsung", 899, "rosegold", "Android");
 
-SamsungGalaxyS6.printInfo();
 
 var OnePlusOne = new Phone("OnePlus", 1840, "black", "Android");
 
-OnePlusOne.printInfo();
 
 
-Phone.prototype.getWarrantyCost = function() {
-	console.log(" The warranty cost in case of "  + this.brand + " is " + this.price*0.10)
+Phone.prototype.printInfo = function() {
+console.log("The phone brand is " + this.brand + ", color is " + this.color + " ,price is " + this.price + " the operatingSystem is " + this.operatingSystem + " and the warranty cost is " + this.price*0.10)
 }
 
 
-SamsungGalaxyS6.getWarrantyCost();
-OnePlusOne.getWarrantyCost();
-iPhone6S.getWarrantyCost();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
+iPhone6S.printInfo();
 
 
